7b0d334a7223751c3acbbe91c442bb0995d425c1
sheilallee <git@stackblitz.com>
2024-12-12 07:42:53 -0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import {Component} from '@angular/core';
+import {Usuario} from '../shared/model/usuario';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  titulo = 'Social IFPB';
+  aluno: string = 'Sheila Lima Lee';
+
+  constructor() {
+  }
+
+}
+
+
+
